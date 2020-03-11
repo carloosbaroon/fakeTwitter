ebc87ba0bd83ff55f8457e1e8ba80a8cf038f916
carloosbaroon <46849851+carloosbaroon@users.noreply.github.com>
2020-03-11 07:26:53 -0600

    Trying to recover data from master

---
+++ src/main/java/org/websparrow/StrutsJsonDTO.java
@@ -0,0 +1,21 @@
+package org.websparrow;
+
+public class StrutsJsonDTO {
+	String state, country;
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
