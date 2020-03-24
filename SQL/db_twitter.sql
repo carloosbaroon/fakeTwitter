e7e20ebc676190e828db30d65c6d73d7a48561d2
carloosbaroon <46849851+carloosbaroon@users.noreply.github.com>
2020-03-24 07:48:31 -0600

    script SQL

---
+++ SQL/db_twitter.sql
@@ -0,0 +1,41 @@
+drop database twitter;
+create database twitter;
+use twitter;
+
+CREATE TABLE Usuario
+(
+ id_usuario Int NOT NULL AUTO_INCREMENT,
+ name    Varchar(45) NOT NULL ,
+ mail     Varchar(45) NOT NULL ,
+ password Varchar(45) NOT NULL ,
+
+PRIMARY KEY (id_usuario)
+);
+
+
+CREATE TABLE Comentario
+(
+  id_comentario Int NOT NULL AUTO_INCREMENT,
+ id_usuario   Int NOT NULL ,
+ fecha  Date        NOT NULL ,
+ contenido     Varchar(45) NOT NULL ,
+
+PRIMARY KEY (id_comentario)
+
+);
+
+
+CREATE TABLE Respuesta
+(
+ id_respuesta  Int NOT NULL ,
+ fecha Date   NOT NULL ,
+ contenido Varchar(45)    NOT NULL ,
+ id_comentario Int NOT NULL ,
+ id_usuario Int     NOT NULL ,
+
+PRIMARY KEY (id_respuesta)
+);
+
+Alter table Comentario add Foreign Key (id_usuario) references Usuario (id_usuario);
+Alter table Respuesta add Foreign Key (id_usuario) references Usuario (id_usuario);
+Alter table Respuesta add Foreign Key (id_comentario) references Comentario (id_comentario);
